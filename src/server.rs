af81634aa9099fdf4645802823ab563c2a750c83
TamirCohen <tamir@tamirc.com>
2021-02-15 13:44:01 +0200

    Add vector of all clients

--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,29 @@
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+
 
 struct ChatServer{
     listener: TcpListener,
-    clients: Vec<TcpStream>
+    clients: Arc<Mutex<Vec<TcpStream>>>
 }
 
-impl ChatServer{
-    
-    async fn handle_client(mut socket: TcpStream)
+struct Client{
+    socket: TcpStream,
+    clients: Arc<Mutex<Vec<TcpStream>>>
+}
+
+impl Client{
+
+    async fn handle_client(&mut self)
     {
         let mut buf = vec![0; 1024];
+
         
         // In a loop, read data from the socket and write the data back.
         loop {
-            let n = socket
+            let n = self.socket
                 .read(&mut buf)
                 .await
                 .expect("failed to read data from socket");
@@ -25,19 +32,24 @@ impl ChatServer{
                 return;
             }
 
-            socket
+            self.socket
                 .write_all(&buf[0..n])
                 .await
                 .expect("failed to write data to socket");
         }
     }
+
+}
+
+impl ChatServer{
+
     async fn init() -> Result<ChatServer, Box<dyn Error>>
     {
         let listener = TcpListener::bind("127.0.0.1:8080".to_string()).await?; 
-        
+        let clients = Arc::new(Mutex::new(Vec::new()));
         let chat = ChatServer{
             listener,
-            clients: Vec::new()};  
+            clients};
         Ok(chat)
     }
 
@@ -47,8 +59,15 @@ impl ChatServer{
             // Asynchronously wait for an inbound socket.
 
             let (socket, _) = self.listener.accept().await?;
+
+            let clients = Arc::clone(&self.clients);
+            let mut client = Client{
+                socket,
+                clients
+            };
+
             tokio::spawn(async move{
-                    ChatServer::handle_client(socket).await;
+                    client.handle_client().await;
                 }
             );
         }
